976cd828c59c94544e153828a3dcdaf1a5c1e2f2
Benedikt Danner <benedikt@danner.eu.com>
2023-02-09 18:57:29 +0100

    feat: update release

--- release.bat
+++ release.bat
@@ -1,4 +1,1 @@
-cargo release version minor --workspace --execute --no-confirm
-git add .
-git commit -m "chore: update version"
-cargo release --workspace --dependent-version upgrade --execute --no-confirm
+cargo release minor --workspace --dependent-version upgrade --execute --no-confirm
