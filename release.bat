afc68d79f83202efe483720821a906e995be8a4d
Benedikt Danner <benedikt@danner.eu.com>
2023-02-09 18:52:18 +0100

    chore: update version

--- release.bat
+++ release.bat
@@ -1,4 +1,4 @@
 cargo release version minor --workspace --execute --no-confirm
 git add .
 git commit -m "chore: update version"
-cargo release --workspace --dependent-version upgrade --execute
+cargo release --workspace --dependent-version upgrade --execute --no-confirm
